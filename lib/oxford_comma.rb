312ffd2a3103a148e540b351d2d1c78774c4f3d2
Canyon Mathis <canyon.mathis@gmail.com>
2020-01-28 01:31:25 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,7 +1,17 @@
+
+array = ["apple"]
 def oxford_comma(array)
- ["kiwi"].join(",")
+  if array.length == 2
+  array[-2] << " and "
+  array.join
+  elsif array.length == 1
+  	array.join
+  elsif array.length > 2
+    array[-1].prepend "and "
+	array.join(", ")
+  end
 end
 
-
+oxford_comma(array)
 
 
